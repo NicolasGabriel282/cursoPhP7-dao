48949ad5ba4c51c848e4d3a516c9570725f05f2e
Nicolas <nicolasneres@gmail.com>
2023-06-16 20:19:09 -0300

    Adicionada funções select

--- index.php
+++ index.php
@@ -4,9 +4,27 @@ require_once("config.php");
 //$usuarios= $sql->select("SELECT * FROM tb_usuarios");
 
 //echo json_encode($usuarios);
-$root = new Usuario();
-$root->loadbyId(13);
-echo $root;
+
+//Carrega um usuario
+//$root = new Usuario();
+//$root->loadbyId(13);
+//echo $root;
+
+
+//Carrega uma lista de usuarios
+//$lista=Usuario::getList();
+//echo json_encode($lista);
+
+
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("root");
+//echo json_encode($search);
+
+
+//Carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario -> login("user","12345");
+echo $usuario;
 
 
  ?>
